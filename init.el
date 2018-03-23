0f46958e802419bf4b8aad3106b0a8596292e256
LoranceChen <w68055010@qq.com>
2018-03-23 11:54:05 +0800

    update theme

--- init.el
+++ init.el
@@ -25,7 +25,7 @@
 (require 'cl) ;;common lisp package
 (defvar my-packages '(
 		      company
-		      monokai-theme
+		     ;; monokai-theme
 		      ivy
 		      swiper
 		      counsel
@@ -46,13 +46,13 @@
 		      sql-indent
 		      nyan-mode
 		     ;; dracula-theme
-		      spacemacs-theme
+		     ;; spacemacs-theme
 		     ;; rebecca-theme
 		     ;; zenburn-theme
 		     ;; doom-themes
 		     ;; material-theme
-		     ;; moe-theme
-		      solarized-theme
+		      ;; moe-theme
+		     ;; solarized-theme
 		      ) "Default packages")
 
 (defun my-package-installed-p ()
@@ -123,18 +123,29 @@
 
 
 ;;theme
-(load-theme 'spacemacs-dark t) ;;auto complete font is ugly. be used to load font bold at first
+;;;;;;; overview ;;;;;;;;;
+;; doom-vibrant ;;淡青色调，清凉的干净
+;;material ;;藏青色调，冷色，安稳，轻微的drawn
+;;monokai ;;沉稳中透露出热情
+;;rebecca ;;深沉紫，色彩层次不分明
+;;solarized-drak ;; 色彩整体偏低沉，稳重，有点土
+;;spacemacs-dark ;; 层次区分挺好，就是有点陈旧的图书馆的书，适合40岁以后的中年大叔使用
+;;zenburn ;; 军绿色调，回到了80年代
+;;misterioso ;; 就像一个刚毕业的研究生，有思想的沉淀，也有点对新生活的追求
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;(load-theme 'spacemacs-dark t) ;;auto complete font is ugly. be used to load font bold at first
 ;;(load-theme 'monokai t) ;; keyword is red
 ;;(load-theme 'dracula t) ;; keyword is white
-
+;;(load-theme 'zenburn t)
 ;;(load-theme 'rebecca t) ;; auto complete is to dark 
-(load-theme 'solarized-dark t) ;; color to drawn(憔悴的)
+;;(load-theme 'solarized-dark t) ;; color to drawn(憔悴的)
 ;;(load-theme 'material t) ;;don't like yellow keyword
+(load-theme 'misterioso t) ;;对elixir支持不够完善，auto-complete。。。醉了。 todo,代码补全的提示框的颜色搭配需要提个意见
 
 ;; common
   ;; company mode for all file
 (add-hook 'after-init-hook 'global-company-mode)
-
 
 
 
@@ -277,6 +288,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auto-revert-remote-files t)
+ '(custom-safe-themes
+   (quote
+    ("bfdcbf0d33f3376a956707e746d10f3ef2d8d9caa1c214361c9c08f00a1c8409" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "93f9654f91d31e9a9ec6ea2fcffcfcab38353a9588673f2b750e591f704cd633" "4e21fb654406f11ab2a628c47c1cbe53bab645d32f2c807ee2295436f09103c6" "a866134130e4393c0cad0b4f1a5b0dd580584d9cf921617eee3fd54b6f09ac37" "0846e3b976425f142137352e87dd6ac1c0a1980bb70f81bfcf4a54177f1ab495" "b5ecb5523d1a1e119dfed036e7921b4ba00ef95ac408b51d0cd1ca74870aeb14" "53d1bb57dadafbdebb5fbd1a57c2d53d2b4db617f3e0e05849e78a4f78df3a1b" "7666b079fc1493b74c1f0c5e6857f3cf0389696f2d9b8791c892c696ab4a9b64" "2a1b4531f353ec68f2afd51b396375ac2547c078d035f51242ba907ad8ca19da" "b97a01622103266c1a26a032567e02d920b2c697ff69d40b7d9956821ab666cc" "f09acf642ecd837d2691ba05c6f3e1d496f7930b45bf41903e7b37ea6579aa79" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "2af26301bded15f5f9111d3a161b6bfb3f4b93ec34ffa95e42815396da9cb560" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(package-selected-packages (quote (idris-mode ensime ivy racer monokai-theme company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
